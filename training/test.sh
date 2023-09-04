6b884fba5dc745845fade252ca1fa836d9a76706
Carlos Gutierrez <carlos.gutierrez@magmalabs.io>
2023-09-04 12:38:35 -0600

    Improve example of 'Loops'

--- training/test.sh
+++ training/test.sh
@@ -93,11 +93,20 @@ case $COUNTRY in
   ;;
 esac
 
-# Cicles
+# Loops
+# reading from a file
 while read line; do
   response=$(curl --write-out "%{http_code}\n" --silent --output /dev/null "$line")
   echo $line: $response
 done < test.txt
+# based on an increment
+number=1
+while [ $number -lt 10 ]
+do
+  echo "$number"
+  number=$(( number+1 ))
+done
+
 
 # Redirect to file
 echo "Hello" > generated.txt
