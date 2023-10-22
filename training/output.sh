54df843969b364d39fc8426b4d895eaf7bc3d657
Carlos Gutierrez <carlos.gutierrez@magmalabs.io>
2023-10-22 09:19:57 -0600

    A more 'output' related examples

--- training/output.sh
+++ training/output.sh
@@ -1,5 +1,28 @@
 #!/bin/zsh
 
+# Print to screen
+echo "Hello World!"
+# Print without carriage return with the "-n" option
+echo -n "Hello "
+echo "World!"
+# => Hello World!
+
+# String concatenation
+echo "Hello" "World" "Again"
+# => Hello World Again
+# we can use a "back slash"(\) if the line is too long
+echo "Hello" "World" "Again" \
+                     "My" "Friends"
+# => Hello World Again My Friends
+
+# Using "back slash"(\) interpretation within string
+# for that we need to use the "-e" flag
+echo -e "Hello\tWorld\tAgain" # using tabs
+echo -e "Hello\nWorld\nAgain" # using carriage return
+# in some cases the "-e" option is not needed
+echo "Hello World \"Again\"" # using double quotes
+echo "Hello World Again\!" # using exclamation mark (if it's not escaped it has an odd behavior)
+
 # Displaying the result of running something: echo the result of
 # running a command in a subshell $()
 echo $(ifconfig)
