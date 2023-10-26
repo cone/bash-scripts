1eaa8ead72148279a3bb41cf46faa4bb8aa5ff41
Carlos Gutierrez <carlos.gutierrez@magmalabs.io>
2023-10-25 18:25:59 -0600

    Add a few more examples about variables

--- training/variables.sh
+++ training/variables.sh
@@ -1,5 +1,43 @@
 #!/bin/zsh
 
+# Simple variables (automatic type detection)
+myvar="Hello"
+myvar2=10
+# I reference it by prepending "$"
+echo $myvar
+
+# String interpolation
+var1='Hello'
+var2='world!'
+# we need double quotes in order to interpolate the strings
+var3="${var1} ${var2}"
+echo $var3
+# => Hello world!
+# we can combine numbers and strings
+var4=10
+echo "${var1} ${var2}"
+# Hello 10
+# This also works:
+echo "$var1 $var2"
+# => Hello world!
+
+# Built-in variables
+# >>>>>>> HOME variable
+echo $HOME
+# => path/to/my/home
+# I can use it directly in a command:
+ls $HOME
+# >>>>>>> USER variable
+echo $USER
+# => myusername
+# >>>>>>> HOST-NAME variable
+echo $HOST-NAME
+# => myusername.local-NAME
+
+# Avoid the expansion of a variable (escape the $ sign)
+echo "\$HOME"
+# => $HOME
+
 # see all declared variables
 declare -p
 
